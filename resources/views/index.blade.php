190cd7b202a698507e8c57f035a991684c9b7026
Tom Emming <tom@tinux-it.nl>
2025-05-06 22:03:37 +0200

    feat: added database seed on deployment and a notice that the app is reset every 10 minutes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,12 @@
 <x-layout>
     <div class="text-center text-sm mt-4">
         <a href="https://tomemming.nl">Made with ❤️ by Tom Emming</a>
+        <br>
+        <span class="text-xs">
+        <span class="text-blue-500">ℹ️</span> The application is automagically redeployed every 10 minutes
+    </span>
     </div>
+
 
     <livewire:task-list />
 </x-layout>
